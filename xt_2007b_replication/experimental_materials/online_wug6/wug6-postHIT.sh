580877853d9d3328be10ed7461ef945e726fd9b0
Molly Lewis <mll@stanford.edu>
2015-07-08 13:30:10 -0700

    first commit

---
+++ xt_2007b_replication/experimental_materials/online_wug6/wug6-postHIT.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+pushd /Documents/GRADUATE_SCHOOL/Turk/aws-mturk-clt-1.3.0/bin
+./loadHITs.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 -label /Documents/GRADUATE_SCHOOL/Projects/Wug/JavaScript/wug6//wug6 -input /Documents/GRADUATE_SCHOOL/Projects/Wug/JavaScript/wug6//wug6.input -question /Documents/GRADUATE_SCHOOL/Projects/Wug/JavaScript/wug6//wug6.question -properties /Documents/GRADUATE_SCHOOL/Projects/Wug/JavaScript/wug6//wug6.properties -maxhits 1
+popd
